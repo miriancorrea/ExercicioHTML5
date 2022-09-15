678a111f9ff66ec2793ca2ea7f575b52562ce867
miriancorrea <mirian.cristina147@gmail.com>
2022-09-15 20:50:40 -0300

    commit

---
+++ ExemplosJavascript/exemplofuncoes.js
@@ -0,0 +1,11 @@
+function filha(valor1, valor2) {
+
+    return valor1*valor2;
+
+}
+function exemplo(valor) {
+    var texto = 'valor ' + valor;
+  
+    alert(texto);
+    document.write(filha(valor, 10 ));
+}
